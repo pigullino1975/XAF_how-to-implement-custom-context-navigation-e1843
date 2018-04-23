33061bce4ba422ae0c4eb2d730d514d3f48079c5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ContextNavigation.Module/TaskBasedHelpController.cs
@@ -0,0 +1,113 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.NodeWrappers;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Model;
+
+namespace ContextNavigation.Module {
+    public class TaskBasedHelpController : WindowController {
+        public const string ViewId = "HelpDocument_ListView_FewColumns";
+        public const string CriteriaKeyName = "Criteria";
+        private string parentViewClassName;
+        private ShowNavigationItemController showNavigationItemController;
+
+        public TaskBasedHelpController() {
+            this.TargetWindowType = WindowType.Main;
+        }
+
+        protected override void OnFrameAssigned() {
+            UnsubscribeFromEvents();
+            base.OnFrameAssigned();
+            showNavigationItemController =
+                Frame.GetController<ShowNavigationItemController>();
+            if (showNavigationItemController != null) {
+                showNavigationItemController.NavigationItemCreated +=
+                    new EventHandler<NavigationItemCreatedEventArgs>(
+                    showNavigationItemController_NavigationItemCreated);
+                showNavigationItemController.CustomShowNavigationItem +=
+                    new EventHandler<CustomShowNavigationItemEventArgs>(
+                    showNavigationItemController_CustomShowNavigationItem);
+            }
+        }
+
+        void showNavigationItemController_NavigationItemCreated(
+            object sender, NavigationItemCreatedEventArgs e) {
+            ChoiceActionItem navigationItem = e.NavigationItem;
+            IModelObjectView viewNode = ((IModelNavigationItem)e.NavigationItem.Model).View as IModelObjectView;
+            if (viewNode != null) {
+                ITypeInfo objectTypeInfo =
+                    XafTypesInfo.Instance.FindTypeInfo(viewNode.ModelClass.Name);
+                if (objectTypeInfo != null) {
+                    IObjectSpace myObjectSpace = Application.CreateObjectSpace();
+                    int helpDocumentsCount = myObjectSpace.GetObjectsCount(
+                        typeof(HelpDocument), CriteriaOperator.Parse(
+                        "ObjectType == '" + objectTypeInfo.Type.ToString() + "'"));
+                    if (helpDocumentsCount != 0) {
+                        ViewShortcut shortcut = new
+                        ViewShortcut(typeof(HelpDocument), null, ViewId);
+                        shortcut[CriteriaKeyName] = viewNode.ModelClass.Name;
+                        ChoiceActionItem DocumentsNavigationGroup =
+                            new ChoiceActionItem("CustomDocuments",
+                            "Task-Based Help", shortcut);
+                        navigationItem.Items.Add(DocumentsNavigationGroup);
+                    }
+                }
+            }
+        }
+
+        void showNavigationItemController_CustomShowNavigationItem(
+            object sender, CustomShowNavigationItemEventArgs e) {
+            if ((e.ActionArguments.SelectedChoiceActionItem != null) &&
+                e.ActionArguments.SelectedChoiceActionItem.Id == "CustomDocuments") {
+                ChoiceActionItem parent =
+                    e.ActionArguments.SelectedChoiceActionItem.ParentItem;
+                IObjectSpace objectSpace = Application.CreateObjectSpace();
+                CollectionSource collectionSource = new CollectionSource(
+                    objectSpace, typeof(HelpDocument));
+                IModelObjectView parentViewNode = ((IModelNavigationItem)parent.Model).View as IModelObjectView;
+                collectionSource.Criteria.Add("CustomDocumentsController",
+                    CriteriaOperator.Parse("ObjectType == '" +
+                        parentViewNode.ModelClass.Name + "'"));
+                View view = Application.CreateListView(
+                    "HelpDocument_ListView_FewColumns", collectionSource, true);
+                e.ActionArguments.ShowViewParameters.CreatedView = view;
+                parentViewClassName = parentViewNode.ModelClass.Name;
+                view.CustomizeViewShortcut += new
+                EventHandler<CustomizeViewShortcutArgs>(view_CustomizeViewShortcut);
+                e.Handled = true;
+            }
+        }
+
+        void view_CustomizeViewShortcut(object sender, CustomizeViewShortcutArgs e) {
+            if (!string.IsNullOrEmpty(parentViewClassName)) {
+                e.ViewShortcut[CriteriaKeyName] = parentViewClassName;
+            }
+        }
+
+        private void UnsubscribeFromEvents() {
+            if (showNavigationItemController != null) {
+                showNavigationItemController.CustomShowNavigationItem -=
+                    new EventHandler<CustomShowNavigationItemEventArgs>(
+                    showNavigationItemController_CustomShowNavigationItem);
+                showNavigationItemController.NavigationItemCreated -=
+                    new EventHandler<NavigationItemCreatedEventArgs>(
+                    showNavigationItemController_NavigationItemCreated);
+                showNavigationItemController = null;
+            }
+        }
+
+        protected override void Dispose(bool disposing) {
+            UnsubscribeFromEvents();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ VB/ContextNavigation.Module/Updater.vb
@@ -0,0 +1,39 @@
+Imports Microsoft.VisualBasic
+Imports System
+
+Imports DevExpress.ExpressApp.Updating
+Imports DevExpress.Xpo
+Imports DevExpress.Data.Filtering
+Imports DevExpress.ExpressApp
+Imports DevExpress.Persistent.BaseImpl
+
+Namespace ContextNavigation.Module
+	Public Class Updater
+		Inherits ModuleUpdater
+		Public Sub New(ByVal objectSpace As ObjectSpace, ByVal currentDBVersion As Version)
+			MyBase.New(objectSpace, currentDBVersion)
+		End Sub
+		Public Overrides Sub UpdateDatabaseAfterUpdateSchema()
+			MyBase.UpdateDatabaseAfterUpdateSchema()
+
+			Dim sampleDocumentOne As HelpDocument = ObjectSpace.FindObject(Of HelpDocument)(CriteriaOperator.Parse("Title == 'Sample topic one'"))
+			If sampleDocumentOne Is Nothing Then
+				sampleDocumentOne = ObjectSpace.CreateObject(Of HelpDocument)()
+				sampleDocumentOne.Title = "Sample topic one"
+				sampleDocumentOne.ObjectType = GetType(Contact)
+				sampleDocumentOne.Text = "Sample text"
+				sampleDocumentOne.Save()
+			End If
+
+			Dim sampleDocumentTwo As HelpDocument = ObjectSpace.FindObject(Of HelpDocument)(CriteriaOperator.Parse("Title == 'Sample topic two'"))
+			If sampleDocumentTwo Is Nothing Then
+				sampleDocumentTwo = ObjectSpace.CreateObject(Of HelpDocument)()
+				sampleDocumentTwo.Title = "Sample topic two"
+				sampleDocumentTwo.ObjectType = GetType(Task)
+				sampleDocumentTwo.Text = "Sample text"
+				sampleDocumentTwo.Save()
+			End If
+
+		End Sub
+	End Class
+End Namespace
